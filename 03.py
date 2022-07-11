eead52b0c5a9b0e3d922eac1ceefd164edaacdf6
lynn <juno2020k@gmail.com>
2022-07-11 17:12:27 +0900

    3주차 과제

---
+++ 03.py
@@ -0,0 +1,4 @@
+#별찍기
+star = int(input())
+for i in range(star +1):
+    print("*"*i)
---
+++ 04.py
@@ -0,0 +1,8 @@
+# 두 수 비교
+a,b=  map(int,input().split())
+if a > b:
+    print(">")
+elif a == b:
+    print("==") 
+else:
+    print("<") 
